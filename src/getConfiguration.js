31cc9354f4e8e9e389258ec6d25f607dcf638148
satheeshpandianj <52313344+satheeshpandianj@users.noreply.github.com>
2021-11-08 21:11:16 +0530

    Uploaded src folder

---
+++ src/getConfiguration.js
@@ -0,0 +1,72 @@
+/***************************************************************************
+Script Name		: getConfiguration.js
+Date Created	: 12/07/2021
+Author			: Satheesh Pandian
+Description		: Endpoint returning the configuration details for 
+                  all the sales models. Includes details such as 
+                  available variants, soft offers and monthly options
+Request inputs	: 
+                 1. Market 
+                 2. Brand
+Request header	:	
+                 1. VCC-Api-Key
+                 2. Accept
+Method			: GET
+
+***************************************************************************/
+
+//importing the libraries for run the scripts
+import { sleep } from 'k6';
+import http from 'k6/http';
+import { check } from 'k6';
+import { Rate } from 'k6/metrics';
+import { Counter } from 'k6/metrics';
+import { Trend } from 'k6/metrics';
+
+// Create an object to get the error rate
+let errorRate = new Rate('getCarConfigErrorRate');
+let errorCount = new Counter('getCarConfigErrorCount');
+
+// Setting the test configuration along withE threshold value to check the performance
+export let options = {
+    tags: {
+        ENVNAME: __ENV.ENVNAME,
+        PROJECTNAME: __ENV.PROJECTNAME,
+        APINAME: __ENV.APINAME,
+    },
+};
+
+
+export default function () {
+    // Get the URL 
+    let url = 'https://gw.qa.consumer.api.volvocars.com/dcom/financialoptions/configuration?market=DE&brand=VCC';
+
+    //Headers to be passed in the request
+    let params = {
+        headers: {
+            'Accept': 'application/vnd.volvocars.api.dcom.financialoptions.configurationresponse.v2+json',
+            'VCC-Api-Key': '1558dc36508249a3a0d545d3609a1fe9' // API key is for authentication
+        }
+    };
+
+
+    //Hit the payload using GET method
+    let response = http.get(url, params);
+
+    //console.log(response.status);
+
+    //Check the response code is 200 or not
+    check(response, { 'Status Code is 200 ': (result) => result.status === 200 },);
+
+    // Increase the error rate if the response code is not 200
+    // errorRate.add(response.status !== 200);
+    if (response.status !== 200) {
+        errorCount.add(1)
+    }
+
+    // wait for 2 second
+    sleep(2);
+}
+/*************************************************************************************
+                                end of script
+*************************************************************************************/
