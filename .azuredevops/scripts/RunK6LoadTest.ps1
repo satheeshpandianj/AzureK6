8b70a365113e141dcfb90d53fbfc31f2f4583ad7
satheeshpandianj <52313344+satheeshpandianj@users.noreply.github.com>
2021-11-24 12:38:51 +0530

    Updated Stages in commandline

--- .azuredevops/scripts/RunK6LoadTest.ps1
+++ .azuredevops/scripts/RunK6LoadTest.ps1
@@ -25,8 +25,8 @@ param (
     ### Load test resources
     [Parameter(Mandatory = $false)][string]$loadTestIdentifier = $(Get-Date -format "yyyyMMddhhmmss"), #Unique identifier for each run, also used as a folder name within the Share of the storage account
     [Parameter(Mandatory = $false)][string]$loadTestK6Script = "$($env:Build_Repository_LocalPath)\src\$src_script", #The load test file path in K6
-    [Parameter(Mandatory = $false)][string]$loadTestVUS = 3, # The number of concurrent Virtual Users for each container
-    [Parameter(Mandatory = $false)][string]$loadTestDuration = "60s", #The duration of the test in seconds
+    #[Parameter(Mandatory = $false)][string]$loadTestVUS = 3, # The number of concurrent Virtual Users for each container
+    #[Parameter(Mandatory = $false)][string]$loadTestDuration = "60s", #The duration of the test in seconds
     ############## Trial starts
     [Parameter(Mandatory = $false)][string]$loadTestSourcePath = "$($env:Build_Repository_LocalPath)", #The Util path in framework   
    ############## Trial ends
@@ -154,7 +154,7 @@ Write-Host "Creating agents container(s)"
         --image $using:K6AgentImage --restart-policy Never --cpu $using:K6AgentCPU --memory $using:K6AgentMemory `
         --environment-variables AGENT_NUM=$_ LOAD_TEST_ID=$using:loadTestIdentifier `
         --azure-file-volume-account-name $using:storageAccountName --azure-file-volume-account-key $using:storageAccountKey --azure-file-volume-share-name $using:storageShareName --azure-file-volume-mount-path "/$using:AciK6AgentLoadTestHome/" `
-        --command-line "k6 run -e ENV=$using:src_env -e PROJECT=$using:src_project -e APINAME=$using:src_api_name --vus $using:src_users --duration $using:src_test_duration /$using:AciK6AgentLoadTestHome/src/$using:src_script --summary-export /$using:AciK6AgentLoadTestHome/$using:loadTestIdentifier/${using:loadTestIdentifier}_${_}_summary.json --out json=/$using:AciK6AgentLoadTestHome/$using:loadTestIdentifier/${using:loadTestIdentifier}_$_.json" 
+        --command-line "k6 run -e ENV=$using:src_env -e PROJECT=$using:src_project -e APINAME=$using:src_api_name --stage 10s:$using:src_users,$using:src_test_duration:$using:src_users,10s:0 /$using:AciK6AgentLoadTestHome/src/$using:src_script --summary-export /$using:AciK6AgentLoadTestHome/$using:loadTestIdentifier/${using:loadTestIdentifier}_${_}_summary.json --out json=/$using:AciK6AgentLoadTestHome/$using:loadTestIdentifier/${using:loadTestIdentifier}_$_.json" 
 } -ThrottleLimit 10
 
 $injectorsEnd = Get-Date
