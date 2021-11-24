38777270acff2c6cc267ede4993e47e7facaeb24
satheeshpandianj <52313344+satheeshpandianj@users.noreply.github.com>
2021-11-24 14:17:17 +0530

    Reverted all the changes

--- .azuredevops/scripts/RunK6LoadTest.ps1
+++ .azuredevops/scripts/RunK6LoadTest.ps1
@@ -11,7 +11,8 @@ param (
     [Parameter(Mandatory = $true)][string]$src_env,
     [Parameter(Mandatory = $true)][string]$src_project,
     [Parameter(Mandatory = $true)][string]$src_api_name,
-    [Parameter(Mandatory = $true)][string]$src_stages,
+    [Parameter(Mandatory = $true)][string]$src_users,
+    [Parameter(Mandatory = $true)][string]$src_test_duration,
 
     ### Azure resources
     [Parameter(Mandatory = $false)][string]$loadTestResourceGroup = "AzureK6", #The name of the resource group where to create the Azure resources 
@@ -153,7 +154,7 @@ Write-Host "Creating agents container(s)"
         --image $using:K6AgentImage --restart-policy Never --cpu $using:K6AgentCPU --memory $using:K6AgentMemory `
         --environment-variables AGENT_NUM=$_ LOAD_TEST_ID=$using:loadTestIdentifier `
         --azure-file-volume-account-name $using:storageAccountName --azure-file-volume-account-key $using:storageAccountKey --azure-file-volume-share-name $using:storageShareName --azure-file-volume-mount-path "/$using:AciK6AgentLoadTestHome/" `
-        --command-line "k6 run -e ENV=$using:src_env -e PROJECT=$using:src_project -e APINAME=$using:src_api_name --stage $using:src_stages /$using:AciK6AgentLoadTestHome/src/$using:src_script --summary-export /$using:AciK6AgentLoadTestHome/$using:loadTestIdentifier/${using:loadTestIdentifier}_${_}_summary.json --out json=/$using:AciK6AgentLoadTestHome/$using:loadTestIdentifier/${using:loadTestIdentifier}_$_.json" 
+        --command-line "k6 run -e ENV=$using:src_env -e PROJECT=$using:src_project -e APINAME=$using:src_api_name --vus $using:src_users --duration $using:src_test_duration /$using:AciK6AgentLoadTestHome/src/$using:src_script --summary-export /$using:AciK6AgentLoadTestHome/$using:loadTestIdentifier/${using:loadTestIdentifier}_${_}_summary.json --out json=/$using:AciK6AgentLoadTestHome/$using:loadTestIdentifier/${using:loadTestIdentifier}_$_.json" 
 } -ThrottleLimit 10
 
 $injectorsEnd = Get-Date
